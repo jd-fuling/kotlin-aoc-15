12a0942d8c6c6ea21445b062bdf7c89c6c9c09bb
Jørgen <jorgen.drevland@gmail.com>
2023-06-14 13:50:57 +0200

    feat: day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,34 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var floor = 0
+        for (c in input.joinToString("")) {
+            if (c == '(') {
+                floor++
+            } else if (c == ')') {
+                floor--
+            }
+        }
+        return floor
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var floor = 0
+        for ((i, c) in input.joinToString("").withIndex()) {
+            if (c == '(') {
+                floor++
+            } else if (c == ')') {
+                floor--
+            }
+            if (floor < 0) {
+                return i + 1
+            }
+        }
+        return -1
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 0)
 
     val input = readInput("Day01")
     part1(input).println()
